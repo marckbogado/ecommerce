547de5cfa17f6bd97f2455a95f9f9fb24b9b0e59
Marcelo Bogado <marckbogado@gmail.com>
2023-05-28 20:46:11 -0300

    autoload das classes

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -11,6 +11,10 @@ class ComposerStaticInit521571040cdcc3ddb5d0f115ca41b99c
         array (
             'PHPMailer\\PHPMailer\\' => 20,
         ),
+        'H' => 
+        array (
+            'Hcode\\' => 6,
+        ),
     );
 
     public static $prefixDirsPsr4 = array (
@@ -18,10 +22,10 @@ class ComposerStaticInit521571040cdcc3ddb5d0f115ca41b99c
         array (
             0 => __DIR__ . '/..' . '/phpmailer/phpmailer/src',
         ),
-    );
-
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+        'Hcode\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+        ),
     );
 
     public static $prefixesPsr0 = array (
@@ -50,7 +54,6 @@ class ComposerStaticInit521571040cdcc3ddb5d0f115ca41b99c
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit521571040cdcc3ddb5d0f115ca41b99c::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit521571040cdcc3ddb5d0f115ca41b99c::$prefixDirsPsr4;
-            $loader->fallbackDirsPsr4 = ComposerStaticInit521571040cdcc3ddb5d0f115ca41b99c::$fallbackDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit521571040cdcc3ddb5d0f115ca41b99c::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit521571040cdcc3ddb5d0f115ca41b99c::$classMap;
 
